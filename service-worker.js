980e3200ebcf49d4f09dc3eb66ce392c16303e54
Micko Widi <widimicko85@gmail.com>
2020-10-11 12:06:39 +0700

    add Workbox

--- service-worker.js
+++ service-worker.js
@@ -1,81 +1,61 @@
-const CACHE_NAME = "englandLeague-v3";
-let urlsToCache = [
-    "/",
-    "/css/materialize.min.css",
-    "/css/style.css",
-    "/img/contact/fb.png",
-    "/img/contact/ig.png",
-    "/img/contact/tgram.png",
-    "/img/contact/wa.png",
-    "/img/icons/icon1024.png",
-    "/img/icons/icon144.png",
-    "/img/icons/icon192.png",
-    "/img/icons/icon48.png",
-    "/img/icons/icon512.png",
-    "/img/icons/icon72.png",
-    "/img/icons/icon96.png",
-    "/js/notification.js",
-    "/js/api.js",
-    "/js/db.js",
-    "/js/idb.js",
-    "/js/jquery.min.js",
-    "/js/materialize.min.js",
-    "/js/nav.js",
-    "/js/swregist.js",
-    "/pages/about.html",
-    "/pages/favourite.html",
-    "/pages/home.html",
-    "/pages/team.html",
-    "/index.html",
-    "/manifest.json",
-    "nav.html",
-    "https://fonts.googleapis.com/icon?family=Material+Icons",
-    "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-    "https://unpkg.com/snarkdown@1.0.2/dist/snarkdown.umd.js"
-];
+importScripts(
+	'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js'
+);
 
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
+if (workbox) {
+    console.log(`Workbox Loaded`);
+    workbox.precaching.precacheAndRoute(
+        [
+            {url: "/", revision: "1"},
+            {url: "/css/materialize.min.css", revision: "1"},
+            {url: "/css/style.css", revision: "1"},
+            {url: "/img/contact/fb.png", revision: "1"},
+            {url: "/img/contact/ig.png", revision: "1"},
+            {url: "/img/contact/tgram.png", revision: "1"},
+            {url: "/img/contact/wa.png", revision: "1"},
+            {url: "/img/icons/icon1024.png", revision: "1"},
+            {url: "/img/icons/icon144.png", revision: "1"},
+            {url: "/img/icons/icon192.png", revision: "1"},
+            {url: "/img/icons/icon48.png", revision: "1"},
+            {url: "/img/icons/icon512.png", revision: "1"},
+            {url: "/img/icons/icon72.png", revision: "1"},
+            {url: "/img/icons/icon96.png", revision: "1"},
+            {url: "/js/notification.js", revision: "1"},
+            {url: "/js/app.js", revision: "1"},
+            {url: "/js/api.js", revision: "1"},
+            {url: "/js/db.js", revision: "1"},
+            {url: "/js/idb.js", revision: "1"},
+            {url: "/js/jquery.min.js", revision: "1"},
+            {url: "/js/materialize.min.js", revision: "1"},
+            {url: "/js/nav.js", revision: "1"},
+            {url: "/js/swregist.js", revision: "1"},
+            {url: "/pages/about.html", revision: "1"},
+            {url: "/pages/favourite.html", revision: "1"},
+            {url: "/pages/home.html", revision: "1"},
+            {url: "/pages/team.html", revision: "1"},
+            {url: "/index.html", revision: "1"},
+            {url: "/manifest.json", revision: "1"},
+            {url: "nav.html", revision: "1"},
+            {url: "https://fonts.googleapis.com/icon?family=Material+Icons", revision: "1"},
+            {url: "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2", revision: "1"},
+            {url: "https://unpkg.com/snarkdown@1.0.2/dist/snarkdown.umd.js", revision: "1"}
+        ],
+        {
+            ignoreUrlParametersMatching: [/.*/]
+        }
+    );
+    
+    
+    workbox.routing.registerRoute(
+        new RegExp('https://api.football-data.org/v2/'),
+        workbox.strategies.networkFirst({
+            cacheName: 'EnglandLeague'
         })
     );
-});
-
-self.addEventListener("fetch", (event) => {
-    let base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then((cache) => {
-                return fetch(event.request).then((response) => {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, { ignoreSearch: true }).then((response) => {
-                return response || fetch(event.request);
-            })
-        )
-    }
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName != CACHE_NAME) {
-                        console.log(`ServiceWorker: cache ${cacheName} dihapus`);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+}
+else {
+    console.log(`Workbox fail to load`);
+} 
 
 self.addEventListener('push', (event) => {
     let body;
